69039c5e71a41cdf6821b13a0e66880a5fe994bd
ram1219 <kimgaram0320@naver.com>
2020-07-06 14:15:08 +0900

    20200706

--- StudyCSharp/HelloApp/Program.cs
+++ StudyCSharp/HelloApp/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static System.Console;		// Console class 안의 데이터를 사용할 때, Consol. 없이 바로 사용 가능 
 
 namespace HelloApp
 {
@@ -10,6 +7,17 @@ namespace HelloApp
 	{
 		static void Main(string[] args)
 		{
+			if (args.Length == 0)
+			{
+				Console.WriteLine("ex: HelloApp.exe <이름>");
+				return;
+			}
+			else
+			{															// Consol.WriteLine("Hello, {0}!", args[0]);	--> 이전형식
+				WriteLine($"Hello, {args[0]}!");						// $를 사용하여 {}안에 받을 데이터를 바로 입력한다.
+				// cmd 창에서 HelloApp.exe 가있는 폴더의 경로로 이동  ->  실행
+				// 속성 -> 디버그 -> 명령줄 인쇄에 입력 -> 실행
+			}
 		}
 	}
 }
