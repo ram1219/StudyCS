68cb23794d6230e67d7d5e30c993c8aa1f3236e3
ram1219 <kimgaram0320@naver.com>
2020-07-06 14:23:21 +0900

    수정

--- StudyCSharp/StudyString/Properties/AssemblyInfo.cs
+++ StudyCSharp/StudyString/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 어셈블리에 대한 일반 정보는 다음 특성 집합을 통해 
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
-[assembly: AssemblyTitle("HelloApp")]
+[assembly: AssemblyTitle("StudyString")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HelloApp")]
+[assembly: AssemblyProduct("StudyString")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
-[assembly: Guid("0f00a86f-5180-44f4-8b1b-c1ae63360598")]
+[assembly: Guid("59781f4b-e08c-455b-bf8c-e934e37febaf")]
 
 // 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
 //
