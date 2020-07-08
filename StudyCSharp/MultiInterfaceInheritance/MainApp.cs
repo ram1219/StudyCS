ccdcb534b088f73fa8758b3af03ce6ce5dd2d3d2
ram1219 <kimgaram0320@naver.com>
2020-07-08 10:03:06 +0900

    다중상속클래스

--- StudyCSharp/MultiInterfaceInheritance/MainApp.cs
+++ StudyCSharp/MultiInterfaceInheritance/MainApp.cs
@@ -22,7 +22,8 @@ namespace MultiInterfaceInheritance
 		public string Company;
 		public float HorsePower;
 	}
-
+	
+	// Vehicle, IRunnable, IFlyable을 다중 상속받는 FlyingCar 클래스
 	class FlyingCar : Vehicle, IRunnable, IFlyable
 	{
 		public void Run()
@@ -40,11 +41,14 @@ namespace MultiInterfaceInheritance
 	{
 		static void Main(string[] args)
 		{
+			// 다중 상속받은 클래스
 			FlyingCar car = new FlyingCar();
 			car.Run();
 			car.Fly();
 			car.Company = "현대";
 
+			// 부모 인터페이스 
+			// 부모연산자인 IRunnable에 as 연산자를 사용하여 자식 클래스인 car에 캐스팅 하였다.
 			IRunnable runnable = car as IRunnable;
 			runnable.Run();
 
